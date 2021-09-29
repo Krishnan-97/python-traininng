1bc8fa92655dc2ee2d17eff6b15f84f3eef8473a
Krishnan <krishnandx@gmail.com>
2021-09-29 10:01:36 +0530

    name_year.py & rock_paper.py updated

--- name_year.py
+++ name_year.py
@@ -3,11 +3,16 @@ import datetime
 name = input("Enter Name:")
 age = int(input("Enter Age:"))
 
-dt = datetime.datetime.today()
-year = dt.year
+def getYear(age):
+    dt = datetime.datetime.today()
+    year = dt.year
 
-gap = 100-age
+    gap = 100 - age
+
+    return year+gap
+
+ans = getYear(age)
 
 print('Hi',name,'!!')
-print('You will become 100 years old in the year',year+gap)
+print('You will become 100 years old in the year',ans)
 
--- rock_paper.py
+++ rock_paper.py
@@ -2,29 +2,32 @@ while True:
     p1 = input("Player1 Enter(rock,paper,scissor):")
     p2 = input("Player2 Enter(rock,paper,scissor):")
 
-    if(p1 == 'rock'):
-        if(p2 == 'paper'):
-            print("Player2 wins!!")
-        elif(p2 == 'scissor'):
-            print('Player1 wins!!')
-        elif(p2 == 'rock'):
-            print('Draw!!')
+    def game(p1,p2):
+        if (p1 == 'rock'):
+            if (p2 == 'paper'):
+                print("Player2 wins!!")
+            elif (p2 == 'scissor'):
+                print('Player1 wins!!')
+            elif (p2 == 'rock'):
+                print('Draw!!')
 
-    elif(p1 == 'paper'):
-        if(p2 == 'paper'):
-            print("Draw!!")
-        elif(p2 == 'scissor'):
-            print('Player2 wins!!')
-        elif(p2 == 'rock'):
-            print('Player1 Wins!!')
+        elif (p1 == 'paper'):
+            if (p2 == 'paper'):
+                print("Draw!!")
+            elif (p2 == 'scissor'):
+                print('Player2 wins!!')
+            elif (p2 == 'rock'):
+                print('Player1 Wins!!')
 
-    elif(p1 == 'scissor'):
-        if(p2 == 'paper'):
-            print("Player1 wins!!")
-        elif(p2 == 'scissor'):
-            print('Draw!!')
-        elif(p2 == 'rock'):
-            print('Player2 wins!!')
+        elif (p1 == 'scissor'):
+            if (p2 == 'paper'):
+                print("Player1 wins!!")
+            elif (p2 == 'scissor'):
+                print('Draw!!')
+            elif (p2 == 'rock'):
+                print('Player2 wins!!')
+
+    game(p1,p2)
 
     p = input("Do you want to play again??(y/n)")
     if(p == 'y'):
